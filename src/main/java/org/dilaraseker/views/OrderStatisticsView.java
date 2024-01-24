048caa27ee1847105f925dfd9f72c5b8d9524a9f
DilaraSeker <Dilara@DESKTOP-BI2GG8E>
2024-01-24 05:31:39 +0300

    Show total amount

---
+++ src/main/java/org/dilaraseker/views/OrderStatisticsView.java
@@ -0,0 +1,12 @@
+package org.dilaraseker.views;
+
+import java.util.Map;
+
+
+public class OrderStatisticsView {
+
+    public void displayTotalAmount(double totalAmount) {
+        System.out.println("   Tüm siparişlerdeki malların toplam tutarı: " + totalAmount + " TL");
+        System.out.println();
+    }
+}
