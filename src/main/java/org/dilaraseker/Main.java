e0cd943f60c44f08cdc1c45cbef453187f83c6c9
DilaraSeker <Dilara@DESKTOP-BI2GG8E>
2024-01-24 05:32:53 +0300

    Control calculation and showing statistics

--- src/main/java/org/dilaraseker/Main.java
+++ src/main/java/org/dilaraseker/Main.java
@@ -1,17 +1,21 @@
 package org.dilaraseker;
 
+import org.dilaraseker.controllers.OrderStatisticsController;
+import org.dilaraseker.models.OrderDataReader;
+import org.dilaraseker.views.OrderStatisticsView;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Order data file path
+        String filePath = "src/main/resources/orders.json";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        OrderDataReader orderDataReader = new OrderDataReader(filePath);
+        OrderStatisticsView orderStatisticsView = new OrderStatisticsView();
+        OrderStatisticsController orderStatisticsController = new OrderStatisticsController(orderDataReader, orderStatisticsView);
+
+        orderStatisticsController.processOrders();
+
     }
 }
---
+++ src/main/java/org/dilaraseker/controllers/OrderStatisticsController.java
@@ -0,0 +1,37 @@
+package org.dilaraseker.controllers;
+
+import org.dilaraseker.models.Order;
+import org.dilaraseker.models.OrderDataReader;
+import org.dilaraseker.views.OrderStatisticsView;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class OrderStatisticsController {
+    private OrderDataReader orderDataReader;
+    private OrderStatisticsView orderStatisticsView;
+
+    public OrderStatisticsController(OrderDataReader orderDataReader, OrderStatisticsView orderStatisticsView) {
+        this.orderDataReader = orderDataReader;
+        this.orderStatisticsView = orderStatisticsView;
+    }
+
+    public void processOrders() {
+        // Read data from JSON file and convert it into Order objects
+        List<Order> orders = orderDataReader.readOrders();
+
+        // Select only data on specific order numbers
+        List<Order> selectedOrders = orders.stream()
+                .filter(order -> order.getOrderNumber() == 1000 || order.getOrderNumber() == 1001 || order.getOrderNumber() == 1002)
+                .collect(Collectors.toList());
+
+        // Statistical calculations
+        OrderStatisticsCalculator statisticsCalculator = new OrderStatisticsCalculator(selectedOrders);
+        double totalAmount = statisticsCalculator.calculateTotalAmountForAllOrder();
+
+        // View
+        orderStatisticsView.displayTotalAmount(totalAmount);
+
+    }
+}
